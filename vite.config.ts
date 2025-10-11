3b6be21b7a1258d6ab44f8a4a7ab7d549adb1f71
tijn <tijn@tijn.dev>
2025-10-11 19:47:02 +0200

    refactor: remove custom server port

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
 	plugins: [
 		tsConfigPaths(),
 		tanstackStart(),
