e17e8f756d5a4873c0469612d7a0d15b06071c53
tijn <tijn@tijn.dev>
2025-10-13 22:24:18 +0200

    refactor: use bits-ui  for the button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,44 +1,49 @@
 <script module lang="ts">
-	import type { WithElementRef } from '$lib/utils'
-	import type { Snippet } from 'svelte'
-	import type {
-		HTMLAnchorAttributes,
-		HTMLButtonAttributes,
-	} from 'svelte/elements'
+	import type { ButtonRootProps } from 'bits-ui'
+	import { tv, type VariantProps } from 'tailwind-variants'
 
-	export type ButtonProps = WithElementRef<HTMLButtonAttributes> &
-		WithElementRef<HTMLAnchorAttributes> & {
-			variant?: 'primary' | 'secondary'
-			size?: 'icon'
-			children?: Snippet
-			href?: string
-		}
+	export const buttonVariants = tv({
+		slots: {
+			base: [
+				'flex items-center justify-center gap-2 rounded-full transition-transform ',
+				'active:scale-90 active:opacity-50',
+			],
+		},
+		variants: {
+			variant: {
+				primary: 'bg-white text-zinc-800',
+				secondary: 'bg-zinc-100/10',
+			},
+			size: {
+				default: 'px-4 py-2',
+				icon: 'size-10',
+			},
+		},
+		defaultVariants: {
+			size: 'default',
+			variant: 'primary',
+		},
+	})
+
+	export type ButtonProps = ButtonRootProps &
+		VariantProps<typeof buttonVariants>
 </script>
 
 <script lang="ts">
+	import { Button } from 'bits-ui'
 	import { cn } from 'cnfn'
 
 	const {
-		variant = 'primary',
+		children,
+		variant,
 		size,
 		class: className,
-		children,
-		href,
-		...rest
+		...props
 	}: ButtonProps = $props()
+
+	const classes = buttonVariants({ variant, size })
 </script>
 
-<svelte:element
-	this={href ? 'a' : 'button'}
-	{href}
-	{...rest}
-	class={cn(
-		'flex items-center justify-center gap-2 rounded-full transition-transform active:scale-90 active:opacity-50',
-		variant === 'primary' && 'bg-white text-zinc-800',
-		variant === 'secondary' && 'bg-zinc-100/10',
-		size === 'icon' ? 'size-10' : 'h-9 px-4',
-		className,
-	)}
->
+<Button.Root class={classes.base({ class: cn(className) })} {...props}>
 	{@render children?.()}
-</svelte:element>
+</Button.Root>
